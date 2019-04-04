6400a956e49db124667f6aecdc540678ffcf1e69
Clara Lepesqueur <37105449+Intenable@users.noreply.github.com>
2019-04-04 11:43:06 +0200

    Update Main.java

--- Projet JAVA/src/Main.java
+++ Projet JAVA/src/Main.java
@@ -1,18 +1,24 @@
-public class Main {
+package NotesElevesProfesseurs;
 
-    public static void main(String[] args) {
+public class Main {
+	
+	public static void main(String[] args) 
+	{
         //Eleve eleve = new Eleve("Gustave", "Mehaddi");
         //System.out.println(eleve.toString());
 
-         Professeur professeur = new Professeur("Soleil","Tournesol");
+        Professeur professeur = new Professeur("Soleil","Tournesol");
         System.out.println(professeur.toString());
         Eleve eleve = new Eleve("Toto", "test", 12, 3, 2019);
-        Evaluation evaluation = new Evaluation(eleve, professeur, "maths", 0);
-        eleve.mediane(evaluation);
-
+       for(int i = 0; i < 5; i++) {
+        	eleve.evaluations.add(new Evaluation(eleve, professeur, "matiere", i*4));
+       }
+       System.out.println("Moyenne : " + eleve.moyenne(eleve));
+       System.out.println("Médiane : " + eleve.mediane(eleve));
        //Evaluation evaluation = new Evaluation(eleve,professeur,"Mathématiques",8);
        //System.out.println(evaluation);
-
-
-    }
+	}
+	
+	
 }
+
