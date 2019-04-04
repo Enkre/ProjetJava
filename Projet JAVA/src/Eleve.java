7118932b83f15939b0bf9102a76a61d7048a476b
Clara Lepesqueur <37105449+Intenable@users.noreply.github.com>
2019-04-04 11:41:56 +0200

    Update Eleve.java

--- Projet JAVA/src/Eleve.java
+++ Projet JAVA/src/Eleve.java
@@ -1,43 +1,72 @@
-public class Eleve extends NomPrenom {
+package NotesElevesProfesseurs;
+import java.util.ArrayList;
 
-    private int id;
-    private int jour;
-    private int mois;
-    private int annee;
-    Evaluation evaluation;
-    private static final int NB_EVALUATIONS = 10;
+public class Eleve extends NomPrenom 
+{
 
-    //Constructeur d'initialisation
-    public Eleve(String prenom, String nom, int jour, int mois, int annee)
-    {
-        super(prenom,nom);
-        this.jour=jour;
-        this.mois=mois;
-        this.annee=annee;
-    }
+	private int id;
+	private int jour;
+	private int mois;
+	private int annee;
+	ArrayList<Evaluation> evaluations;
+	public static final int NB_EVALUATIONS = 10;
 
-    //Accesseur pour l'identifiant
-    public int getId(){
-        return id;
-    }
-
-    public void setId(){
-        this.id=id;
-    }
-    
-    public double mediane(Evaluation evaluation)
+	//Constructeur d'initialisation
+	public Eleve(String prenom, String nom, int jour, int mois, int annee)
 	{
-		double note;
-		note = evaluation.getNote();
-		System.out.println(note);
-		return note;
+		super(prenom,nom);
+	    this.jour = jour;
+	    this.mois = mois;
+        this.annee = annee;
+        evaluations = new ArrayList<>();
 	}
+	//Accesseur pour l'identifiant
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId()
+	{
+		this.id = id;
+    }
+	
+	static double moyenne(Eleve eleve) {
+		int somme = 0;
+		try {
+	        for (Evaluation eval : eleve.evaluations) {
+	        	somme += eval.getNote();
+	        }
+		} catch(IllegalStateException e) {
+			System.out.println("Aucune note n'a été saisie !");
+		} catch(ArythmeticException e) {
+			System.out.println("Division par 0");
+			// Add evaluation
+		}
+        return somme/eleve.evaluations.size();
+	}
+	
+	public double mediane(Eleve eleve)
+	{
+		int compteur = 0;
+		for (Evaluation eval : eleve.evaluations) {
+			compteur ++;
+		}
+		if (compteur%2 == 0) {
+			compteur = compteur/2;
+		}
+		else {
+			compteur = ((compteur - 1)/ 2) + 1;
+		}
+		return eleve.evaluations.get(compteur - 1).getNote();
+		
+	}
+	
+	
 
     @Override // Décris un élève
     public String toString()
     {
         return super.toString();
-    }
-
-
+	}
 }
