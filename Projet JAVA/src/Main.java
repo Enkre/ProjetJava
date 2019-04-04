d6ca2d8331e824ffcdf7f74950e776e4b9bd3241
Clara Lepesqueur <37105449+Intenable@users.noreply.github.com>
2019-04-04 10:12:12 +0200

    Update Main.java

--- Projet JAVA/src/Main.java
+++ Projet JAVA/src/Main.java
@@ -4,8 +4,11 @@ public class Main {
         //Eleve eleve = new Eleve("Gustave", "Mehaddi");
         //System.out.println(eleve.toString());
 
-        Professeur professeur = new Professeur("Soleil","Tournesol");
+         Professeur professeur = new Professeur("Soleil","Tournesol");
         System.out.println(professeur.toString());
+        Eleve eleve = new Eleve("Toto", "test", 12, 3, 2019);
+        Evaluation evaluation = new Evaluation(eleve, professeur, "maths", 0);
+        eleve.mediane(evaluation);
 
        //Evaluation evaluation = new Evaluation(eleve,professeur,"Mathématiques",8);
        //System.out.println(evaluation);
