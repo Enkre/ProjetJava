6908cd4fd0e64d898aec82887bcbc1e2c434d2f5
Clara Lepesqueur <37105449+Intenable@users.noreply.github.com>
2019-04-26 12:04:53 +0200

    Update Promotion.java

--- Projet JAVA/src/Promotion.java
+++ Projet JAVA/src/Promotion.java
@@ -1,2 +1,30 @@
+package NotesElevesProfesseurs;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Promotion {
+	private String nom;
+	private List<Eleve> eleves;
+	
+	//Constructeur d'initialisation
+	public Promotion() {
+		this.nom = nom;
+		this.eleves = new ArrayList<>();
+	}
+	
+	//Accesseur pour le nom	
+	public String getNom()
+	{
+		return nom;
+	}
+	public void setNom(String nom)
+	{
+		this.nom = nom;
+    }
+	
+	//Accesseur en lecture des élèves qui retourne une copie
+	public List<Eleve> getEleves() 
+	{ 
+		return new ArrayList<>(eleves);
+	}
 }
