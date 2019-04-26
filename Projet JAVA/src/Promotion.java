b9e6106a0350545284a33645e4575742cd826c73
Clara Lepesqueur <37105449+Intenable@users.noreply.github.com>
2019-04-26 21:03:25 +0200

    Update Promotion.java

--- Projet JAVA/src/Promotion.java
+++ Projet JAVA/src/Promotion.java
@@ -3,36 +3,43 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Promotion {
-	private String nom;
-	private List<Eleve> eleves;
-	
-	//Constructeur d'initialisation
-	public Promotion() {
-		this.nom = nom;
-		this.eleves = new ArrayList<>();
-	}
-	
-	//Accesseur pour le nom	
-	public String getNom()
-	{
-		return nom;
-	}
-	public void setNom(String nom)
-	{
-		this.nom = nom;
+    private String nom;
+    private List<Eleve> eleves;
+
+    //Constructeur d'initialisation
+    public Promotion(String nom) {
+        this.nom = nom;
+        this.eleves = new ArrayList<>();
     }
-	
-	//Accesseur en lecture des élèves qui retourne une copie
-	public List<Eleve> getEleves() 
-	{ 
-		return new ArrayList<>(eleves);
-	}
-	
-	public Eleve rechercher(int id)
+
+    //Accesseur pour le nom
+    public String getNom()
+    {
+        return nom;
+    }
+    public void setNom(String nom)
+    {
+        this.nom = nom;
+    }
+
+    public void add (Eleve eleve)
+    {
+        eleves.add(eleve);
+        eleve.setPromotion(this);
+    }
+
+    //Accesseur en lecture des élèves qui retourne une copie
+    public List<Eleve> getEleves()
+    {
+        return new ArrayList<>(eleves);
+    }
+
+    //Méthode qui permet de rechercher un élève dans la promotion en fonction de son id
+    public Eleve rechercher(int id)
     {
         for (Eleve eleve : eleves)
         {
-            if (equals(eleve) == true) {
+            if (eleve.equals(eleve) == true) {
                 return eleve;
             }
         }
