a95f177ceb6c8ac4d1a7348f51b2366ef44c0d44
Clara Lepesqueur <37105449+Intenable@users.noreply.github.com>
2019-04-26 14:55:09 +0200

    Update Professeur.java

--- Projet JAVA/src/Professeur.java
+++ Projet JAVA/src/Professeur.java
@@ -12,6 +12,22 @@ public class Professeur extends NomPrenom{
     {
         return promotion.rechercher(id);
     }
+    
+    public void setNote(Promotion promotion, int id, double note, int indice) throws IllegalStateException
+    {
+        Eleve eleve = rechercher(promotion, id);
+        Evaluation eval = null;
+        if (eleve == null) {
+            throw new IllegalStateException("Eleve inexistant dans la promotion !");
+        }
+        try {
+            eval = eleve.getEvaluation().get(indice);
+            eval.setNote(note);
+            eval.setProfesseur(this);
+        } catch (IndexOutOfBoundsException error) {
+            //eval = new Evaluation(eleve, this, matiere, note);
+        }
+    }
 
     @Override // Décris un professeur
     public String toString()
