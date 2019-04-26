2c6ef23a97875d47c551e75a90a6ee491399cc30
Clara Lepesqueur <37105449+Intenable@users.noreply.github.com>
2019-04-26 21:02:03 +0200

    Update Evaluation.java

--- Projet JAVA/src/Evaluation.java
+++ Projet JAVA/src/Evaluation.java
@@ -2,14 +2,14 @@ package NotesElevesProfesseurs;
 
 import java.util.ArrayList;
 
-public class Evaluation {
+public class Evaluation implements Comparable {
 
-	private String matiere;
+    private String matiere;
     private double note;
     Eleve eleve;
     Professeur professeur;
 
-    //constructeur d'initialisation
+    //Constructeur d'initialisation
     public Evaluation(Eleve eleve, Professeur professeur, String matiere, double note)
     {
         this.eleve = eleve;
@@ -17,36 +17,60 @@ public class Evaluation {
         this.matiere = matiere;
         this.note = note;
     }
-    
-    
+
+    //Méthode qui permet d'ajouter des évaluations en fonction du nombre max autorisé
+    public void addEvaluation()
+    {
+        if (eleve.evaluations.size() <= eleve.NB_EVALUATIONS) {
+            eleve.evaluations.add(this);
+        }
+        else {
+            System.out.println("Le nombre maximum d'évaluation (" + eleve.NB_EVALUATIONS + ") par élève a été dépassé !");
+        }
+    }
+
     //Accesseurs en lecture et écriture
     public double getNote()
-	{
-		return note;
-	}
-	public void setNote(double note)
-	{
-		this.note = note;
+    {
+        return note;
     }
-	
-	public String getMatiere()
-	{
-		return matiere;
-	}
-	public void setMatiere(String matiere)
-	{
-		this.matiere = matiere;
+    public void setNote(double note)
+    {
+        this.note = note;
     }
-	
-	public Professeur getProfesseur()
-	{
-		return professeur;
-	}
-	public void setProfesseur(Professeur professeur)
-	{
-		this.professeur = professeur;
-	}
 
+    public String getMatiere()
+    {
+        return matiere;
+    }
+    public void setMatiere(String matiere)
+    {
+        this.matiere = matiere;
+    }
+
+    public Professeur getProfesseur()
+    {
+        return professeur;
+    }
+    public void setProfesseur(Professeur professeur)
+    {
+        this.professeur = professeur;
+    }
+
+    @Override
+    public int compareTo(Object obj)
+    {
+        Evaluation evaluation = (Evaluation)obj;
+        double diff = this.note - evaluation.getNote();
+        /* For Ascending order*/
+        if (diff < 0) {
+            return -1;
+        }
+        if (diff > 0) {
+            return 1;
+        }
+        return 0;
+    }
 
     @Override // Décris un élève
     public String toString()
