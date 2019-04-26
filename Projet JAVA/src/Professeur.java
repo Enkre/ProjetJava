84f4310c4fb611ab2bce8aa223bca5a1895f8ca8
Clara Lepesqueur <37105449+Intenable@users.noreply.github.com>
2019-04-26 14:22:38 +0200

    Update Professeur.java

--- Projet JAVA/src/Professeur.java
+++ Projet JAVA/src/Professeur.java
@@ -6,6 +6,12 @@ public class Professeur extends NomPrenom{
     {
         super(prenom,nom);
     }
+    
+    //Méthode qui permet au professeur de rechercher un élève dans la promotion en fonction de son id
+    public Eleve rechercher(Promotion promotion, int id) 
+    {
+        return promotion.rechercher(id);
+    }
 
     @Override // Décris un professeur
     public String toString()
